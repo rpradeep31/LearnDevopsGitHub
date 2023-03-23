ed1f1f83fe3c636ee25126485d5a587b25b2e11f
Pradeep Rajasekar <r.pradeep@live.com>
2023-03-23 02:40:32 -0500

    My First API application

---
+++ CURDopertiondotnetCore/CURDopertiondotnetCore/Controllers/BrandController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CURDopertiondotnetCore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors;
+
+namespace CURDopertiondotnetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly BrandContext _brandContext;
+        public BrandController(BrandContext brandContext)
+        {
+            _brandContext = brandContext;
+        }
+        [HttpGet]
+        [EnableCors("AllowOrigin")]
+        public async Task<ActionResult<IEnumerable<Brand>>> GetBrands()
+        {
+            if (_brandContext.Brands == null)
+            {
+                return NotFound();
+            }
+            return await _brandContext.Brands.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Brand>> GetBrands(int id)
+        {
+            if (_brandContext.Brands == null)
+            {
+                return NotFound();
+            }
+            var brand = await _brandContext.Brands.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            return brand;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Brand>> PostBrand(Brand brand)
+        {
+            _brandContext.Brands.Add(brand);
+            await _brandContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBrands), new { id = brand.ID }, brand);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Brand>> putBrand(int id, Brand brand)
+        {
+            if (id!= brand.ID)
+            {
+                return BadRequest();
+            }
+            _brandContext.Entry(brand).State = EntityState.Modified;
+            try
+            {
+                await _brandContext.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                if (!Brandavailable(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+                
+            }
+            return Ok();
+        }
+        private bool Brandavailable (int id)
+        {
+            return (_brandContext.Brands?.Any(x => x.ID == id)).GetValueOrDefault();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteBrand(int id)
+        {
+            if(_brandContext.Brands==null)
+            {
+                return NotFound();
+            }
+            var brand = await _brandContext.Brands.FindAsync(id);
+            if (brand==null)
+            {
+                return NotFound();
+            }
+            _brandContext.Brands.Remove(brand);
+            await _brandContext.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ CURDopertiondotnetCore/CURDopertiondotnetCore/Models/Brand.cs
@@ -0,0 +1,12 @@
+﻿namespace CURDopertiondotnetCore.Models
+{
+    public class Brand
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public int IsActive { get; set; }
+
+
+    }
+}
